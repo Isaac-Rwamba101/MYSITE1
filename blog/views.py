6c9feee597fa7b35db7bb0e66449b8d84ede6c0e
Isaac-Rwamba101 <isaacrwamba019@gmail.com>
2025-06-05 14:56:07 +0300

    updated files

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.http import Http404
 
 
 # Create your views here.
-def post_detail(request, id):
+def post_detail(request, year,month, day, post):
         post = get_object_or_404(
               Post,
-              id=id,
-              status=Post.Status.PUBLISHED
+              status=Post.Status.PUBLISHED,
+              slug=post,
+                publish__year=year,
+                publish__month=month,
+                publish__day=day   # Example year, adjust as needed
         )
         return render(
             request,
@@ -17,7 +21,11 @@ def post_detail(request, id):
         )
         
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Show 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page',1)
+    posts = paginator.get_page(page_number)  
     context = {'posts': posts}
 
     return render(request, 'blog/post/list.html', context)
